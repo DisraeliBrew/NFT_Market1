bfc96559fae5897bdecf8ca29ac6341120257aa6
Reno Raksi <adriano.raksi@gmail.com>
2023-05-01 14:38:55 -0500

    Mint tokens

--- nft_market/frontend/pages/create.jsx
+++ nft_market/frontend/pages/create.jsx
@@ -1,5 +1,6 @@
 import NftCreator from "../components/nftCreator";
+import { abi as MarketContractAbi } from '../../backend/artifacts/contracts/MarketContract.sol/MarketContract.json';
 
 export default function create() {
-    return ( <NftCreator> </NftCreator> );
+    return ( <NftCreator contractAddress="0x2FD4cc38dDCA30Cc27dfb3abc235510e6aB6Ea9d" abi={MarketContractAbi} /> );
 }
