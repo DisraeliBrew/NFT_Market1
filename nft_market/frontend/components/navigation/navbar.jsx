c401897508a4c69ac0eca82306f5fd7a516d506a
Reno Raksi <adriano.raksi@gmail.com>
2023-05-07 13:26:48 -0500

    :lipstick: Moved links stylings

--- nft_market/frontend/components/navigation/navbar.jsx
+++ nft_market/frontend/components/navigation/navbar.jsx
@@ -2,20 +2,26 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 import styles from "../../styles/Navbar.module.css";
 import Link from 'next/link';
 import Image from 'next/image';
+
 export default function Navbar() {
-	return (
-		
-		<nav className={styles.navbar}>
-		<Image
-			src="https://upload.wikimedia.org/wikipedia/commons/e/e5/Wisconsin_Badgers_logo.svg"
-			alt="Logo"
-			width={32}
-			height={32}
-		/>
-		<ConnectButton></ConnectButton>
-			<Link href = '/create'>Mint your NFT</Link>
-			<Link href = '/'>Explore NFTs</Link>
-			<Link href = '/marketplace'>Browse Marketplace</Link>
-		</nav>
-	);
+  return (
+
+      <nav className={styles.navbar}>
+        <Link href="/">
+					<Image
+							src="https://upload.wikimedia.org/wikipedia/commons/e/e5/Wisconsin_Badgers_logo.svg"
+							alt="Logo"
+							width={32}
+							height={32}
+							className={styles.alchemy_logo}
+					/>
+				</Link>
+				<div className={styles.links}>
+					<Link href='/create'>Mint your NFT</Link>
+					<Link href='/'>Explore NFTs</Link>
+					<Link href='/marketplace'>Browse Marketplace</Link>
+					<ConnectButton />
+				</div>
+      </nav>
+  );
 }
